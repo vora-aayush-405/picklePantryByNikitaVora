90296ff81ecc9400804c3d8859973d118b0b933d
aayushv <voraayush@gmail.com>
2026-03-04 02:58:45 +0530

    updated cors rule

--- backend/server.py
+++ backend/server.py
@@ -222,7 +222,7 @@ app.include_router(api_router)
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-    allow_origins=os.environ.get('CORS_ORIGINS', '*').split(','),
+    allow_origins=os.environ.get('CORS_ORIGINS').split(','),
     allow_methods=["*"],
     allow_headers=["*"],
 )
